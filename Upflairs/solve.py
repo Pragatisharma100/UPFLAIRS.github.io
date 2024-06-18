7e970b4a3e83f5530ae0f9edbeb9820b4e4fb598
Trendline <157017872+Pragatisharma100@users.noreply.github.com>
2024-06-18 15:27:06 +0530

    Delete Upflairs directory

--- Upflairs/solve.py
+++
@@ -1,6 +0,0 @@
-# from logic import ls, sorting
-# print(ls)
-# print(sorting(ls))
-
-import logic
-print(logic.sorting(logic.ls))
--- Upflairs/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-ls=[53,87,65,43,29,10,26,39]
-# def apan_min(ls):
-#     min=ls[0]
-#     for i in range (len(ls)):
-#         if min>ls[i]:
-#             min=ls[i]
-#         return min
-# print(apan_min(ls))
